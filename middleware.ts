07b5d0f60e49a8c37ac1d693a21420cdb6f422a5
Efstathios Vranakis <fvranakis@gmail.com>
2025-11-26 12:10:01 +0200

    Another one.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export const runtime = "nodejs";
-
 const isProtectedRoute = createRouteMatcher([
   "/offer-ride(.*)",
   "/my-rides(.*)",
